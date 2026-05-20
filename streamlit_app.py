8ab9ab326a020af9fa0cabe7f17641ad960b8543
mnectoux-kpc <maxime.nectoux@kpconsulting.fr>
2026-05-20 15:09:09 +0000

    7

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 # Import python packages.
-import requests
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -46,3 +45,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+    
+    import requests  
+    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+    st.text(smoothiefroot_response)
